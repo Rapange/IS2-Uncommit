4921b6bce9214cd49e6a2660545951ab359c524f
DanielaFe7GitHub <daniela.milon@ucsp.edu.pe>
2016-11-10 10:22:06 -0500

    #3Segundo intento de Grafico Estadistico

--- lib/component/administrator_sales/product_reportOut_component.dart
+++ lib/component/administrator_sales/product_reportOut_component.dart
@@ -1,13 +1,14 @@
 import 'package:angular2/angular2.dart';
 import 'package:logistic_ui/model.dart';
 import 'package:logistic_ui/providers.dart';
-
+import 'package:logistic_ui/request.dart';
 
 
 @Component(
     selector: 'product-out-report',
     templateUrl: 'product_reportOut_component.html',
-    viewProviders: const [LOGISTIC_SERVICE_PROVIDERS])
+    viewProviders: const [LOGISTIC_SERVICE_PROVIDERS],
+    directives: const [ProductOutReportComponent])
 
 class ProductOutReportComponent {
   bool submitted = false;
